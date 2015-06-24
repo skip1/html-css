9fc65f48e44d0fb1700f0e7b1c882dee928ddc88
“Shmuel <smkipper@gmail.com>
2015-06-24 14:44:30 -0400

    added form transitions

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -3,4 +3,19 @@ $(function () {
         alert("ll");
         $('#slide').slideToggle("fast", "linear");
     });
+    $("input[type='text']").focusin(function () {
+        $(this).css("background-color", "palegoldenrod");
+    });
+     $("input[type='text']").focusout(function () {
+        $(this).css("background-color", "white");
+    });
+    $("#submit").on("click", function () {
+        if ($("#name").val() === '') {
+            alert("Please enter a name");
+        } else if ($("#password").val() === '') {
+            alert ("Please enter a password");
+            } else if ($("#password").val().length < 5) {
+                alert("Password must be at least of 5 characters");
+        };
+    });
 });
